07bca2404c90019ecc87a9457d00314793c21f09
hayeong28 <hayeong28@naver.com>
2025-07-20 21:03:30 +0900

    feat: API 연동

---
+++ frontend/src/api/__generated__/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './APIApi';
+export * from './AuthApi';
+export * from './BranchControllerApi';
+export * from './CommitControllerApi';
+export * from './DocControllerApi';
+export * from './SaveControllerApi';
---
+++ frontend/src/api/apiClient.ts
@@ -0,0 +1,51 @@
+import {
+  APIApi,
+  AuthApi,
+  BranchControllerApi,
+  CommitControllerApi,
+  Configuration,
+  DocControllerApi,
+  SaveControllerApi,
+} from "./__generated__"
+
+export const BACKEND_API = "http://3.34.159.207:8080"
+
+const customFetch = async (url: string, init?: RequestInit) => {
+  //   const loginInfo = getLoginInfo()
+
+  //   // 기존 헤더와 새로운 Authorization 헤더를 병합
+  //   const headers = {
+  //     ...(loginInfo?.token
+  //       ? {
+  //           Authorization: `Bearer ${loginInfo.token}`,
+  //         }
+  //       : {}),
+  //     ...init?.headers,
+  //   }
+
+  // 새로운 init 객체 생성
+  const newInit = {
+    ...init,
+    // headers,
+  }
+
+  // 원래의 fetch 함수 호출
+  return fetch(url, newInit)
+}
+
+// API 클라이언트 설정
+const config = new Configuration({
+  basePath: BACKEND_API, // "http://localhost:8080",
+  credentials: "include",
+  fetchApi: customFetch,
+})
+
+// 모든 API 클라이언트를 하나의 객체로 통합
+export const apiClient = {
+  auth: new AuthApi(config),
+  user: new APIApi(config),
+  branch: new BranchControllerApi(config),
+  commit: new CommitControllerApi(config),
+  document: new DocControllerApi(config),
+  save: new SaveControllerApi(config),
+}
