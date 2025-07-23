5574051d753a09bb23122f009198380d42e3a9f9
hayeong28 <hayeong28@naver.com>
2025-07-23 21:01:26 +0900

    fix: 8080 제거

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -8,9 +8,7 @@ import {
   SaveControllerApi,
 } from "./__generated__"
 
-export const BACKEND_API = import.meta.env.DEV
-  ? ""
-  : "https://3.34.159.207:8080"
+export const BACKEND_API = import.meta.env.DEV ? "" : "https://3.34.159.207"
 
 const customFetch = async (url: string, init?: RequestInit) => {
   // 새로운 init 객체 생성
