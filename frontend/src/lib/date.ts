7200494e3e1bbbf782185e2677fc1a9fc9a0b433
hayeong28 <hayeong28@naver.com>
2025-08-03 22:04:54 +0900

    chore: space 제거

--- frontend/src/lib/date.ts
+++ frontend/src/lib/date.ts
@@ -20,11 +20,14 @@ export const formatDateForDocuments = (dateInput?: string | Date) => {
 
   const date = typeof dateInput === "string" ? new Date(dateInput) : dateInput
 
-  return date.toLocaleDateString("ko-KR", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-  })
+  return date
+    .toLocaleDateString("ko-KR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    })
+    .replace(/\. /g, ".")
+    .replace(/\.$/, "")
 }
